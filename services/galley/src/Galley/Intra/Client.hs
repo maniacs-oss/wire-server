4a60751e2865064a206fc421bf46f946561b3e2c
Jon Elverkilde <jon@wire.com>
2017-12-15 14:37:23 +0100

    Remove more redundant imports

--- services/galley/src/Galley/Intra/Client.hs
+++ services/galley/src/Galley/Intra/Client.hs
@@ -6,27 +6,15 @@ module Galley.Intra.Client
 
 import Bilge hiding (options, getHeader, statusCode)
 import Bilge.RPC
-import Bilge.Retry
 import Galley.App
 import Galley.Intra.Util
-import Galley.Options
 import Galley.Types (UserClients)
-import Control.Lens (view)
-import Control.Retry
-import Data.ByteString (ByteString)
 import Data.ByteString.Char8 (intercalate)
 import Data.ByteString.Conversion
 import Data.Id
-import Data.Misc (portNumber)
-import Data.Text.Encoding (encodeUtf8)
-import Data.Word (Word16)
 import Network.HTTP.Types.Method
 import Network.HTTP.Types.Status
 import Network.Wai.Utilities.Error
-import Util.Options
-
-import qualified Data.ByteString.Lazy as LB
-import qualified Data.Text.Lazy as LT
 
 lookupClients :: [UserId] -> Galley UserClients
 lookupClients uids = do
